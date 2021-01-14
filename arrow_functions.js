1519128684dadf6ba19d75a1b7ace56e9618a264
Herbert F. Silva <hbtfsilva@prontomail.com>
2021-01-13 22:11:45 -0300

    vars

---
+++ arrow_functions.js
@@ -0,0 +1,16 @@
+// var dobreDoValor = function (numero) {
+//     return numero * 2
+// }
+
+// console.log(dobreDoValor(7))
+// -------------------------------------------
+// var dobreDoValor = numero => numero * 2
+
+// ----------------------------
+
+var dobreDoValor = numero => {
+    let resultado = numero * 2
+    return resultado
+}
+
+console.log(dobreDoValor(7))
---
+++ variavel.js
@@ -0,0 +1,37 @@
+// var serie = 'Friends'
+
+// if(true){
+//     serie = 'Game Of Thrones'
+// }
+
+// console.log(serie)
+
+// ----------------------------
+
+// var serie = 'Friends'
+
+// if(true){
+//     var serie2 = 'Game Of Thrones'
+// }
+
+// console.log(serie2)
+
+// ----------------------------
+
+// var serie = 'Friends'
+
+// if(true){
+//     let serie = 'Game Of Thrones'
+//     console.log(serie)
+// }
+// console.log(serie)
+
+// ----------------------------
+const serie = 'Friends'
+serie = 'The Walking Dead'
+
+if(true){
+    let serie = 'Game Of Thrones'
+    console.log(serie)
+}
+console.log(serie)
